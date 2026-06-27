504224e131a8137187ba69c2ef854ec69387d20f
Tawin Tangsukson <79807@student.amnuaysilpa.ac.th>
2026-06-27 15:46:24 +0700

    Fix star flickering in space background
    - Store star properties (size, brightness) to prevent random changes per frame
    - Stars now maintain consistent appearance while animating

--- scripts/space_background.gd
+++ scripts/space_background.gd
@@ -1,22 +1,27 @@
 extends Node2D
 
-var stars: Array[Vector2] = []
+var stars: Array[Dictionary] = []
 var star_speed: float = 50.0
 var star_count: int = 200
 
 func _ready():
 	for i in range(star_count):
-		stars.append(Vector2(randf_range(0, 1280), randf_range(0, 720)))
+		stars.append({
+			"position": Vector2(randf_range(0, 1280), randf_range(0, 720)),
+			"size": randf_range(1, 3),
+			"brightness": randf_range(0.5, 1.0)
+		})
 
 func _process(delta):
-	for i in range(stars.size()):
-		stars[i].y += star_speed * delta
-		if stars[i].y > 720:
-			stars[i] = Vector2(randf_range(0, 1280), 0)
+	for star in stars:
+		star["position"].y += star_speed * delta
+		if star["position"].y > 720:
+			star["position"] = Vector2(randf_range(0, 1280), 0)
 	queue_redraw()
 
 func _draw():
 	for star in stars:
-		var size = randf_range(1, 3)
-		var brightness = randf_range(0.5, 1.0)
-		draw_circle(star, size, Color(brightness, brightness, brightness))
+		var pos = star["position"]
+		var size = star["size"]
+		var brightness = star["brightness"]
+		draw_circle(pos, size, Color(brightness, brightness, brightness))
